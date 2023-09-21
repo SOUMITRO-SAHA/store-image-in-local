d059138a6ca9584f60e2a6bde6143056c350cf95
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-09-21 18:37:14 +0530

    Error Resolved

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import imageRoutes from './routes/image.routes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // view
 
